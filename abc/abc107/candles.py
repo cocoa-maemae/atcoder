b469c2b5077f5cbd75f277f19ccf2debc6aed6cb
cocoa-maemae <breezin_ahimusa@yahoo.co.jp>
2019-08-21 03:07:01 +0200

    add comment

--- abc/abc107/candles.py
+++ abc/abc107/candles.py
@@ -1,4 +1,10 @@
-    N, K = map(int, input().split())
-    x = list(map(int, input().split()))
-    ans = min(b - a + min(abs(a), abs(b)) for a, b in zip(x, x[K-1:]))
-    print(ans)
+from pprint import pprint
+N, K = map(int, input().split())
+x = list(map(int, input().split()))
+
+# mapping [X0...] [Xk-1...] by zip
+#for a, b in zip(x, x[K-1:]):
+#    print(a, b)
+# the answer is the minimum number of Xi+k−1 − Xi + min(|Xi|,|Xi+k−1|) 
+ans = min(b - a + min(abs(a), abs(b)) for a, b in zip(x, x[K-1:]))
+print(ans)

c3b9343a689f7801eb57395d7ba0fa692e9cb327
Tomoki Maeda <breezin_ahimusa@yahoo.co.jp>
2019-08-29 15:45:00 +0900

    Update shift_only.py

--- abc/abc081/shift_only.py
+++ abc/abc081/shift_only.py
@@ -4,5 +4,8 @@ N = int(input())
 A = list(map(int, input().split()))
 ans = float("inf")
 for a in A:
-    ans = min(ans, len(bin(a)) - bin(a).rfind("1") - 1)
+    # If target number is 8(=0b1000), divided count by 2 is 3. 
+    # It is same as the number of 0 of binary expression
+    divi_cnt_by_2 = len(bin(a)) - bin(a).rfind("1") - 1
+    ans = min(ans, divi_cnt_by_2)
 print(round(ans))

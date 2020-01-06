264b0a0ad5506868d2ba62a99c717b02b69c3051
cocoamaemae <cocoamaemae@cocoamaemae-no-MacBook-Air.local>
2020-01-06 19:50:14 +0900

    fix logic

--- abc/abc105/base_2numbers.py
+++ abc/abc105/base_2numbers.py
@@ -3,6 +3,6 @@ a = []
 if N == 0:
     a = ["0"]
 while N != 0:
-    a.append(str(N % 2)) # N mod 2
-    N = -(N - N % 2) // 2 # (N − (N mod 2)) // 2
+    a.append(str(N % 2)) # N mod 2 is same as N mod -2 because remainder is 1 or 0
+    N =  -(N // 2)
 print("".join(list(reversed(a))))

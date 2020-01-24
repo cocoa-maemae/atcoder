b8762ec24c987f6f60e2f023bd7a64284fcda9e7
cocoamaemae <breezin_ahimusa@yahoo.co.jp>
2020-01-25 00:44:29 +0900

    fix logic

--- abc/abc106/105.py
+++ abc/abc106/105.py
@@ -5,10 +5,10 @@ ans = 0
 # target is only odd number
 for o in range(1, N + 1, 2):
     cnt = 0
-    for n in range(1, 1 + floor(sqrt(o))):
+    for n in range(1, o + 1):
         if o % n == 0:
             # number of measure is number of square root * 2
-            cnt += 2
+            cnt += 1
     if cnt == 8:
         ans += 1
 print(ans)

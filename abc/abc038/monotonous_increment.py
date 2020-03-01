bfff90dbbcdcf67aac67077d75e8c67eabbbd7ad
cocoa-maemae <breezin_ahimusa@yahoo.co.jp>
2020-03-01 20:59:26 +0900

    add ABC038

---
+++ abc/abc038/monotonous_increment.py
@@ -0,0 +1,17 @@
+N = int(input())
+A = map(int, input().split())
+tmp, pre, ans = 0, 0, 0
+for a in A:
+  if pre < a:
+    tmp += 1
+  else:
+    tmp = 1
+  pre = a
+  ans += tmp
+print(ans)
+"""
+E.g.
+a   1 2 3 2 1
+tmp 1 2 3 1 1
+ans 1 3 6 7 8
+"""

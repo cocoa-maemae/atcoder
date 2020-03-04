cb8eef4e6dee883d1aa8011dbf865ddd6a60a982
cocoa-maemae <breezin_ahimusa@yahoo.co.jp>
2020-03-04 10:07:59 +0900

    rename

--- abc/abc032/retu.py
+++ abc/abc032/retu.py
@@ -4,15 +4,15 @@ if 0 in S:
   print(N)
   exit()
 # two pointers
-prod, ans, right_i = 1, 0, 0
+prod, ans, fast_i = 1, 0, 0
 for i in range(N):
-  while right_i < N:
-    prod *= S[right_i]
+  while fast_i < N:
+    prod *= S[fast_i]
     if prod > K:
       # cache the index
-      right_i += 1
+      fast_i += 1
       break
-    ans = max(ans, right_i - i + 1)
-    right_i += 1
+    ans = max(ans, fast_i - i + 1)
+    fast_i += 1
   prod //= S[i]
 print(ans)

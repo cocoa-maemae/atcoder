6d6b441c0a61b0821576911885975a7ad87442ec
cocoa-maemae <breezin_ahimusa@yahoo.co.jp>
2020-06-29 01:13:52 +0900

    add comment

--- abc/abc038/monotonous_increment.py
+++ abc/abc038/monotonous_increment.py
@@ -1,18 +1,29 @@
+# greedy
 # similar like ABC152 C - Low Elements
 N = int(input())
-A = map(int, input().split())
-tmp, pre, ans = 0, 0, 0
-for a in A:
-  if pre < a:
+A = list(map(int, input().split()))
+ans, tmp = 1, 1
+for i in range(1, N):
+  if A[i - 1] < A[i]:
     tmp += 1
   else:
     tmp = 1
-  pre = a
   ans += tmp
 print(ans)
 """
+condition is l<=r, so l=r case must be counted.
+
 E.g.
-a   1 2 3 2 1
 tmp 1 2 3 1 1
 ans 1 3 6 7 8
+
+If A=[1,2,3]
+(i)If includes 3 numbers
+(l,r)=(1,3)
+
+(ii)If includes 2 numbers
+(l,r)=(1,2),(2,3)
+
+(iii)If includes 1 number
+(l,r)=(1,1),(2,2),(3,3)
 """

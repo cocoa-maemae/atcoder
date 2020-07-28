b792150373ea03585ab12f37597b1c6865ede3ff
cocoa-maemae <breezin_ahimusa@yahoo.co.jp>
2020-07-28 09:49:34 +0900

    add logics

--- abc/abc061/many_formulas.py
+++ abc/abc061/many_formulas.py
@@ -1,13 +1,3 @@
-"""
-solution by dfs
-
-S = list(input()
-def dfs(S, i, last_s, sum): 
-  if i == len(S):
-    return sum + int(last_s)
-  return dfs(S, i + 1, S[i], int(last_s) + sum) + dfs(S, i + 1, last_s + S[i], sum)
-print(dfs(S, 1, S[0], 0))
-"""
 
 # solution by bit full search
 S = input()

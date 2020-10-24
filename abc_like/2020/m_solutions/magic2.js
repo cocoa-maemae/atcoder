fa9b51ffdb21c22d5108a590675fc250e8a6b4c8
cocoa-maemae <breezin_ahimusa@yahoo.co.jp>
2020-10-24 22:40:56 +0900

    remove semicolon

--- abc_like/2020/m_solutions/magic2.js
+++ abc_like/2020/m_solutions/magic2.js
@@ -1,14 +1,14 @@
 const main = args => {
-  let [[A, B, C], [K]] = args.trim().split('\n').map(r=>r.split` `.map(v=>v|0));
-  let cnt = 0;
+  let [[A, B, C], [K]] = args.trim().split('\n').map(r=>r.split` `.map(v=>v|0))
+  let cnt = 0
   while (A>=B) {
-    B *= 2;
-    cnt++;
+    B *= 2
+    cnt++
   }
   while (B>=C) {
-    C *= 2;
-    cnt++;
+    C *= 2
+    cnt++
   }
-  console.log(cnt > K ? 'No' : 'Yes');
+  console.log(cnt > K ? 'No' : 'Yes')
 }
 main(require('fs').readFileSync('/dev/stdin', 'utf-8'))

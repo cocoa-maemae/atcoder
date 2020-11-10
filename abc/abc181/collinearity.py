c3311db952a78a3a555223db00cd1d97467dee45
cocoa-maemae <breezin_ahimusa@yahoo.co.jp>
2020-11-11 00:48:25 +0900

    fix logic

--- abc/abc181/collinearity.py
+++ abc/abc181/collinearity.py
@@ -5,7 +5,7 @@ for i in range(N):
   for j in range(i + 1, N):
     x1, y1 = coordinates[i]
     x2, y2 = coordinates[j]
-    slope = "0" if x1 == x2 else (y2 - y1) / (x2 - x1)
+    slope = float('inf') if x1 == x2 else (y2 - y1) / (x2 - x1)
     if slope in slopes:
       print("Yes")
       exit()

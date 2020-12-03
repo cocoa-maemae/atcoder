c761f0ffe8b981d4245b3244437cf41cd18a72c9
cocoa-maemae <breezin_ahimusa@yahoo.co.jp>
2020-12-03 10:26:29 +0900

    fix logic

--- abc/abc170/forgidden_list.py
+++
@@ -1,8 +0,0 @@
-X, N = map(int, input().split())
-P = list(map(int, input().split()))
-for d in range(X + 1):
-  for s in [-1, 1]:
-    a = X + s * d
-    if P.count(a) == 0:
-      print(a)
-      exit(0)

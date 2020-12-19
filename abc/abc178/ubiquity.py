49cd5e0eed0d737851e6a72f5be3942b85198407
cocoa-maemae <breezin_ahimusa@yahoo.co.jp>
2020-12-19 20:01:43 +0900

    fix comment

--- abc/abc178/ubiquity.py
+++ abc/abc178/ubiquity.py
@@ -8,10 +8,10 @@ N 0~9
 - includes 0 over 1
 - includes 9 over 1
 
-all the combination 10^N
-combination 0 dosen't exist 9^N
-combination 9 doesn't exist 9^N
-combination 0 and 9 don't exist 8^N
+all the combination 10^N (0~9)
+combination 0 dosen't exist 9^N (1~9)
+combination 9 doesn't exist 9^N (0~8)
+combination 0 and 9 don't exist 8^N (1~8)
 the answer is 10^N-9^N-9^N+8^N
 
 Write Venn diagram

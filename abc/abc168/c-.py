7bc05fafab13aabcb82e5dca599702a735e2f91e
cocoa-maemae <breezin_ahimusa@yahoo.co.jp>
2021-01-29 18:54:12 +0900

    add comment

--- abc/abc168/c-.py
+++ abc/abc168/c-.py
@@ -14,7 +14,12 @@ See https://www.youtube.com/watch?v=MXOSqPzsiqo&feature=youtu.be
 Not need to use Cosine formula
 
 Calculate angle of minutes and hour(2PI=360)
+# Regarding h, 1circle=12h=720min
+# theta h=H*60/720 is mistake, because hour hand moves 1 hour
+# with set of hour and min(1h+60min)
 theta h = (H*60+M)/720*2PI
+
+# Regarding m, 1circle=60min
 theta m = M/60*2PI
 See https://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q1391868678
 

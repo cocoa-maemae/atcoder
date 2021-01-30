b42ace1a90a1fa1b0b3a80f40654714d7ca70834
cocoa-maemae <breezin_ahimusa@yahoo.co.jp>
2021-01-31 00:48:39 +0900

    add comment

--- abc/abc180/takahashi_unevolved.py
+++ abc/abc180/takahashi_unevolved.py
@@ -1,17 +1,16 @@
 X, Y, A, B = map(int, input().split())
-experience = 0
-while A * X <= X + B and A * X < Y:
-    X *= A
-    experience += 1
-experience += (Y - X - 1) // B
+experience, strength = 0, X
+while strength * A <= strength + B and strength * A < Y:
+  X *= A
+  experience += 1
+experience += (Y - X - 1) // B # add rest experience if B is added
 print(experience)
 
-
 """
+To increase experience, consider use A or B,
 If x*A<=x+B, use A
 If x*B<=x+A, use B
 
 If go to Kakomon Gym firstly and then go to AtCoder Gym, X->XA->XA+B
 If go to AtCoder Gym firstly and then go to Kakomon Gym, X->X+B->XA+BA
-
 """
